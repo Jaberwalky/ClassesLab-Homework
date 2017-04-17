2a8f616c6c6a7388ae3cc94697a45ccba9eada97
Paul Jablonski <pauljablonski@me.com>
2017-04-17 16:55:40 +0100

    add all getter methods

--- part_b_spec.rb
+++ part_b_spec.rb
@@ -5,12 +5,21 @@ require 'minitest/rg'
 class TestSportsTeam < Minitest::Test
 
   def setup
-
-
+    @team_members = ["D Gray", "P Hanlon", "L Fontaine", "B McLean", "L Stevenson", "J Forster", "D McGregor", "E Ambrose", "C Crane"]
+    @team = Team.new("Hibernian FC", @team_members, "N Lennon")
   end
 
+  def test_get_name
+    assert_equal("Hibernian FC", @team.get_name())
+  end
 
+  def test_get_players
+    assert_equal(@team_members, @team.get_players)
+  end
 
+  def test_get_coach
+    assert_equal("N Lennon", @team.get_coach())
+  end
 
 
 
