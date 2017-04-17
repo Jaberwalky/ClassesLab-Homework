e4c2b85836ce1a90034329ce12e0f5a37038f842
Paul Jablonski <pauljablonski@me.com>
2017-04-17 16:25:32 +0100

    add get method and tests for student and cohort

--- part_a.rb
+++ part_a.rb
@@ -1,11 +1,16 @@
 class CodeClanStudent
 
-def initialize(name, cohort)
-  @name = name
-  @cohort = cohort
-end
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
 
+  def get_name()
+    return @name
+  end
 
-
+  def get_cohort()
+    return @cohort
+  end
 
 end
--- part_a_spec.rb
+++ part_a_spec.rb
@@ -8,7 +8,13 @@ def setup
   @student_1 = CodeClanStudent.new("Paul", 12)
 end
 
+def test_get_name
+  assert_equal("Paul", @student_1.get_name())
+end
 
+def test_get_cohort
+  assert_equal(12, @student_1.get_cohort())
+end
 
 
 
